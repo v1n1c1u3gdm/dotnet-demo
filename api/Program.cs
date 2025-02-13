322eb1cfe32d01aacbd7d61ca4a497ce4b5af373
Vinicius G. D. Menezes <v1n1c1u3gdm@gmail.com>
2025-02-13 18:33:34 -0300

    Criando estrutura da API, model de usuário e instalando o EF e criando o BD

--- api/Program.cs
+++ api/Program.cs
@@ -1,23 +1,14 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddDbContext<DataContext>(opt=>
+    opt.UseSqlite(builder.Configuration.GetConnectionString("Default"))
+);
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
 app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
