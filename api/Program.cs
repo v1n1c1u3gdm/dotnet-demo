ab95fa8c569154c96760fc372197cc87e624c0b2
Vinicius G. D. Menezes <v1n1c1u3gdm@gmail.com>
2025-02-18 19:30:40 -0300

    Configurando Bootstrap e adicionando propriedades de senha

--- api/Program.cs
+++ api/Program.cs
@@ -4,11 +4,13 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddCors();
 builder.Services.AddDbContext<DataContext>(opt=>
     opt.UseSqlite(builder.Configuration.GetConnectionString("Default"))
 );
 
 var app = builder.Build();
 app.UseHttpsRedirection();
+app.UseCors(config => config.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200, https://localhost:4200"));
 app.MapControllers();
 app.Run();
--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'client';
+  client = inject(HttpClient);
+  users: any;
+
+  ngOnInit(): void {
+    this.client.get('https://localhost:5001/api/Users')
+      .subscribe({
+        next: response => this.users = response,
+        error: error => console.log(error),
+        complete : () =>  {}
+      })
+  }
 }
