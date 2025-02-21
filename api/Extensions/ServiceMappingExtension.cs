8f2f402c49262416a79b4cab9f0109151f3cc721
Vinicius G. D. Menezes <v1n1c1u3gdm@gmail.com>
2025-02-21 16:12:52 -0300

    Limpando a classe de entrada e adicionando bearer tokens válidos no momento aos testes do controller que precisa de autorização agora

---
+++ api/Extensions/ServiceMappingExtension.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Text;
+using API.Data;
+using API.Interfaces;
+using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+
+namespace api.Extensions;
+
+public static class ServiceMappingExtension
+{
+    public static WebApplicationBuilder ConfigureApplicationServices(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddScoped<ITokenService, TokenService>();
+        return builder;
+    }
+
+    public static WebApplicationBuilder ConfigureFrameworkServices(this WebApplicationBuilder builder)
+    {
+        builder.Services
+                .AddCors()
+                .AddDbContext<DataContext>(opt =>
+                    opt.UseSqlite(builder.Configuration.GetConnectionString("Default"))
+                ).AddControllers();
+
+        builder.Services
+                .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(opts =>
+                {
+                    var tokenKey = GetTokenKey(builder);
+                    opts.TokenValidationParameters = ConfigureValidationParameters(tokenKey);
+                });
+
+        return builder;
+    }
+
+    public static WebApplication ConfigureMiddleware(this WebApplication app)
+    {
+        app.UseCors(cfg => cfg.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin())
+            .UseHttpsRedirection();
+
+        app.UseAuthentication().UseAuthorization();
+
+        app.MapControllers();
+
+        return app;
+    }
+
+    private static TokenValidationParameters ConfigureValidationParameters(string tokenKey) =>
+        new()
+        {
+            ValidateIssuerSigningKey = true,
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenKey))
+        };
+    private static string GetTokenKey(WebApplicationBuilder builder) =>
+        builder.Configuration["TokenKey"] ?? throw new Exception("TokenKey property not configured");
+}
--- api/Program.cs
+++ api/Program.cs
@@ -1,68 +1,10 @@
-using System.Text;
-using API.Data;
-using API.Interfaces;
-using API.Services;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
+using api.Extensions;
 
-namespace API
-{
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
-            ConfigureFrameworkServices(builder);
-            ConfigureApplicationServices(builder);
 
-            var app = builder.Build();
-            ConfigureMiddleware(app);
-            app.Run();
-        }
+var builder = WebApplication.CreateBuilder(args)
+    .ConfigureFrameworkServices()
+    .ConfigureApplicationServices();
 
-        private static void ConfigureApplicationServices(WebApplicationBuilder builder)
-        {
-            builder.Services.AddScoped<ITokenService, TokenService>();
-        }
-
-        private static void ConfigureFrameworkServices(WebApplicationBuilder builder)
-        {
-            builder.Services
-                    .AddCors()
-                    .AddDbContext<DataContext>(opt =>
-                        opt.UseSqlite(builder.Configuration.GetConnectionString("Default"))
-                    ).AddControllers();
-
-            builder.Services
-                    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-                    .AddJwtBearer(opts =>
-                    {
-                        var tokenKey = GetTokenKey(builder);
-                        opts.TokenValidationParameters = ConfigureValidationParameters(tokenKey);
-                    });
-        }
-
-        private static void ConfigureMiddleware(WebApplication app)
-        {
-            app.UseCors(cfg => cfg.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin())
-                .UseHttpsRedirection();
-
-            app.UseAuthentication().UseAuthorization();
-
-            app.MapControllers();
-        }
-
-        private static TokenValidationParameters ConfigureValidationParameters(string tokenKey) =>
-            new()
-            {
-                ValidateIssuerSigningKey = true,
-                ValidateIssuer = false,
-                ValidateAudience = false,
-                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenKey))
-            };
-
-        private static string GetTokenKey(WebApplicationBuilder builder) =>
-            builder.Configuration["TokenKey"] ?? throw new Exception("TokenKey property not configured");
-    }
-}
+var app = builder.Build();
+app.ConfigureMiddleware();
+app.Run();
